7e59e056eca176ec0ccbfc34bff4b0c0a11d6950
𝐃𝐡𝐰𝐚𝐧𝐢 𝐒𝐡𝐫𝐢𝐤𝐚𝐫✨ <dhwanishrikar@gmail.com>
2026-04-08 06:56:41 +0530

    Add files via upload

---
+++ pg01.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import cv2
+
+#read grayscale image 
+img1=cv2.imread(r"D:\DHWANI\ENGINEERING\VI\IPCV\ipcv_lab\image.jpg")
+img=cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
+
+#calculate & display the dimentions
+height,width,channels=img.shape
+mh=height//2
+mw=width//2
+
+tl=img[:mh,:mw]
+tr=img[:mh,mw:width]
+bl=img[mh:height,:mw]
+br=img[mh:height,mw:width]
+
+fig,axes=plt.subplots(2,2,figsize=(8,8))
+axes[0,0].imshow(tl)
+axes[0,0].set_title("Top Left")
+axes[0,0].axis("on")
+
+axes[0,1].imshow(tr)
+axes[0,1].set_title("Top Right")
+axes[0,1].axis("on")
+
+axes[1,0].imshow(bl)
+axes[1,0].set_title("Bottom Left")
+axes[1,0].axis("on")
+
+axes[1,1].imshow(br)
+axes[1,1].set_title("Bottom RIght")
+axes[1,1].axis("on")
+
+plt.tight_layout()
+plt.show()
---
+++ pg3.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from skimage.feature import local_binary_pattern
+
+import matplotlib.pyplot as plt
+
+# Step 1: Read image
+image = cv2.imread(r"D:\DHWANI\ENGINEERING\VI\IPCV\ipcv_lab\image.jpg ")
+# Step 2: Convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Step 3: Apply Gaussian Blur
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+# Step 4: Sobel Edge Detection
+sobelx = cv2.Sobel(blur, cv2.CV_64F, 1, 0, ksize=3)
+sobely = cv2.Sobel(blur, cv2.CV_64F, 0, 1, ksize=3)
+sobel_mag = cv2.magnitude(sobelx, sobely)
+# Normalize sobel for better visualization (0 to 255)
+sobel_norm = cv2.normalize(sobel_mag, None, 0, 255,cv2.NORM_MINMAX).astype(np.uint8)
+# Step 5: Apply LBP
+radius = 1
+n_points = 8 * radius
+lbp = local_binary_pattern(blur, n_points, radius, method="uniform")
+# Display results
+plt.figure(figsize=(12, 8))
+plt.subplot(2,2,1)
+plt.title("1. Original Grayscale")
+plt.imshow(gray, cmap='gray')
+plt.axis('off')
+
+plt.subplot(2,2,2)
+plt.title("2. Gaussian Blur (Noise Reduction)")
+plt.imshow(blur, cmap='gray')
+
+plt.axis('off')
+
+plt.subplot(2,2,3)
+plt.title("3. Sobel Edge Magnitude")
+plt.imshow(sobel_norm, cmap='gray')
+plt.axis('off')
+
+plt.subplot(2,2,4)
+plt.title("4. LBP Texture Features")
+plt.imshow(lbp, cmap='gray')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
