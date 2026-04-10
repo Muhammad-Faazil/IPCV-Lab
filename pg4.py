458bd661b888a5031c2a0e59684fda471a14a93b
𝐃𝐡𝐰𝐚𝐧𝐢 𝐒𝐡𝐫𝐢𝐤𝐚𝐫✨ <dhwanishrikar@gmail.com>
2026-04-10 12:00:22 +0530

    Update pg4.py

--- pg4.py
+++ pg4.py
@@ -2,40 +2,36 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread(r"D:\DHWANI\ENGINEERING\VI\IPCV\ipcv_lab\image.jpg")
+image = cv2.imread("image.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-if len(image.shape) == 3:
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-else:
-    gray = image.copy()
+low_contrast = gray // 2 + 50
 
-low_contrast = cv2.convertScaleAbs(gray, alpha=0.5, beta=50)
-
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 enhanced = clahe.apply(low_contrast)
 
-blurred = cv2.GaussianBlur(enhanced, (5, 5), 0)
+blur = cv2.GaussianBlur(enhanced, (5,5), 0)
+ftfff
+_, segmented = cv2.threshold(blur, 0, 255,
+                             cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-ret, segmented = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+plt.figure(figsize=(10,6))
 
-plt.figure(figsize=(15, 5))
+plt.subplot(2,2,1)
+plt.title("Original")
+plt.imshow(gray, cmap='gray')
 
-plt.subplot(1, 3, 1)
-plt.title("1. Low Contrast Image")
+plt.subplot(2,2,2)
+plt.title("Low Contrast")
 plt.imshow(low_contrast, cmap='gray')
-plt.axis('off')
 
-plt.subplot(1, 3, 2)
-plt.title("2. Enhanced (CLAHE)")
+plt.subplot(2,2,3)
+plt.title("Enhanced (CLAHE)")
 plt.imshow(enhanced, cmap='gray')
-plt.axis('off')
 
-plt.subplot(1, 3, 3)
-plt.title("3. Segmented (Otsu)")
+plt.subplot(2,2,4)
+plt.title("Segmented")
 plt.imshow(segmented, cmap='gray')
-plt.axis('off')
 
 plt.tight_layout()
 plt.show()
-
-# Step 9: Stop
